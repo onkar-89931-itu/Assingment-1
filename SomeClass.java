676487982d29730d4332973eab910950ed44a71c
Onkar <89931.kale@students.itu.edu>
2016-04-24 19:24:03 -0700

    AddMultipleevents

--- SomeClass.java
+++ SomeClass.java
@@ -1,14 +1,24 @@
 public class SomeClass{
+	private ArrayList<String> events = new ArrayList<String>();
+	
 publi void thoughtForTheDay(){
-system.out.println("I could'nt repair your brakes, so I made yourgit horn louder");
+system.out.println("I could'nt repair your brakes, so I made your horn louder");
+
+}
 publi void upcomingEvents(){
 system.out.println("Upcomin Events");
-system.out.println("Party at my house tomorrow");
+for (String event : events)
+	System.out.println(event);
+
 }
-}
+
 public static void main (String[]args){
 SomeClass sc = new SomeClass();
 sc.thoughtForTheDay();
+sc.createEvents();
+
 sc.upcomingEvents();
+
+
 }
 }
